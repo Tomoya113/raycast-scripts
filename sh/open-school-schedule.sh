300a7702736ae20610903414292bf20b4b170bbe
txxxxc <tomoya113.code@gmail.com>
2023-04-17 10:04:46 +0900

    [add] open-school-schedule

---
+++ sh/open-school-schedule.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open School Schedule
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description スクールの日程を開く
+# @raycast.author Tomoya Tanaka
+# @raycast.authorURL https://github.com/Tomoya113
+
+open ~/LiT/school_schedule_2023_spring.png
+
